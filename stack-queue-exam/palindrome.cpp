d4fd7bb86a77a18fc6207df0615edab0158812dd
Suhani Jain <suhaanijain.11@gmail.com>
2025-04-30 10:51:33 +0530

    stack-exam

--- stack-queue-exam/palindrome.cpp
+++ stack-queue-exam/palindrome.cpp
@@ -19,32 +19,21 @@ class Stack{
 };
 void Stack::push(int data){
     if(this->top == size - 1){
-        cout << "\n------------------------\n";
-        cout << "Stack is Full!";
-        cout << "\n------------------------\n";
         return;
     }
     this->top++;
     this->arr[this->top] = data;
     this->count++;
-    cout << "\n------------------------\n";
-    cout << "Data Inserted Successfully!";
-    cout << "\n------------------------\n";
+
 }
 void Stack::pop(){
     if(this->top == -1){
-        cout << "\n------------------------\n";
-        cout << "Stack is Empty!";
-        cout << "\n------------------------\n";
         return;
     }
 
     this->arr[this->top] = 0;
     this->top++;
     this->count--;
-    cout << "\n------------------------\n";
-    cout << "Data Removed Successfully!";
-    cout << "\n------------------------\n";
 }
 void Stack::isPalindrome(string name , int size){
     for(int i = 0; i< size;i++){
